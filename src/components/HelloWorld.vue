70edec34e7a0bd95b9743b44a3c80400f91ee28b
sungbum <bumory1987@gmail.com>
2022-09-15 14:29:52 +0900

    page setting

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,20 @@
 <template>
-  <div>
-  <button @click="--pageman">prev page</button><button @click="++pageman">next page</button>
+  <div id="pdfvuer">
+    <div id="buttons" class="ui grey three item inverted bottom fixed menu transition hidden">
+      <a class="item" @click="page > 1 ? page-- : 1">
+        <i class="left chevron icon"></i>
+        Back
+      </a>
+      <a class="ui active item">
+        {{page}} / {{ numPages ? numPages : '∞' }}
+      </a>
+      <a class="item" @click="page < numPages ? page++ : 1">
+        Forward
+        <i class="right chevron icon"></i>
+      </a>
+    </div>
 
-  <pdfvuer src="./static/task.pdf"  :key="pageman" :id="pageman" :page="pageman" style="width:100%;margin:20px auto;">
+  <pdfvuer :src="pdfdata"   :page="page" style="width:100%;margin:20px auto;">
   <template v-slot:loading>
     loading content here...
   </template>
@@ -12,90 +24,45 @@
 
 <script>
 import pdfvuer from 'pdfvuer'
-import {ref ,  onMounted} from 'vue'
+import {ref , onMounted} from 'vue'
 
 export default {
   name: 'HelloWorld',
   components: {
     pdfvuer
   },
-
   setup(){
     const pageman = ref(1)
-    const totalPage = ref(0)
-    const pdfdata = ref('')
-    const other = ref(null)
-    // const other = ref('https://pdftron.s3.amazonaws.com/downloads/pl/demo-annotated.pdf')
-    const getPdf  = async () => {
-        pdfdata.value= 'https://pdftron.s3.amazonaws.com/downloads/pl/demo-annotated.pdf'
-        other.value = await pdfvuer.createLoadingTask('https://pdftron.s3.amazonaws.com/downloads/pl/demo-annotated.pdf')
-        totalPage.value = other.value.numPages
-        console.log(totalPage.value)
+    const numPages = ref(0)
+    const pdfdata = ref(pdfvuer.createLoadingTask(
+          {url :'./static/mi.pdf',
+           }))
 
-
-
-        // pdfdata.value.then(pdf =>{
-        //   numPages.value = pdf.numPages;        })
-        // pdfdata.value = await pdfvuer.createLoadingTask('https://pdftron.s3.amazonaws.com/downloads/pl/demo-annotated.pdf')
-        // console.log( pdfdata.value)
-        // pdfdata.value.then(pdf => {
-        //   self.numPages = pdf.numPages;
-          // window.onscroll = function() { 
-          //   changePage() 
-          //   stickyNav()  
-          // }
-  
-        //   // Get the offset position of the navbar
-        //   var sticky = $('#buttons')[0].offsetTop
-  
-        //   // Add the sticky class to the self.$refs.nav when you reach its scroll position. Remove "sticky" when you leave the scroll position
-        //   function stickyNav() {
-        //     if (window.pageYOffset >= sticky) {
-        //       $('#buttons')[0].classList.remove("hidden")
-        //     } else {
-        //       $('#buttons')[0].classList.add("hidden")
-        //     }
-        //   }
-  
-        //   function changePage () {
-        //     var i = 1, count = Number(pdf.numPages);
-        //     do {
-        //       if(window.pageYOffset >= self.findPos(document.getElementById(i)) && 
-        //           window.pageYOffset <= self.findPos(document.getElementById(i+1))) {
-        //         self.page = i
-        //       }
-        //       i++
-        //     } while ( i < count)
-        //     if (window.pageYOffset >= self.findPos(document.getElementById(i))) {
-        //       self.page = i
-        //     }
-        //   }
-        // });
-    
-    } 
-
+    const page = ref(1)
+    const forward= ()=>{
+      page.value = page.value+1
+      console.log(page.value)
+    }
     onMounted(()=>{
-       getPdf()
+      pdfdata.value.then(pdf=>{
+        numPages.value = pdf.numPages
+        console.log(numPages.value)
+      })
     })
-
-    return {pageman, pdfdata, other }
+ 
+    return {pageman, pdfdata, numPages, page ,forward}
   }
 }
 </script>
 
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style src="pdfvuer/dist/pdfvuer.css"></style>
+<style lang="css" scoped>
+  #buttons {
+    margin-left: 0 !important;
+    margin-right: 0 !important;
+  }
+  /* Page content */
+  .content {
+    padding: 16px;
+  }
 </style>
